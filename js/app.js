30431fabdda36f42d8f370956394528c9d0665f1
DESKTOP-URAJ3BD\namdh <danghoainam1202@gmail.com>
2024-01-31 10:34:42 +0700

    ff

--- js/app.js
+++ js/app.js
@@ -96,7 +96,7 @@ const util = (() => {
       .replace(/'/g, "&#039;");
   };
 
-  const salin = (btn, msg = "Tersalin", timeout = 1500) => {
+  const salin = (btn, msg = "Đã copy", timeout = 1500) => {
     navigator.clipboard.writeText(btn.getAttribute("data-nomer"));
 
     let tmp = btn.innerHTML;
